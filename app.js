260c955ddd282b41c9e558ff1ebe94938eab4302
adam marsh <adammarsh@adams-MacBook-Pro.local>
2019-03-03 12:36:20 -0700

    ehh

--- app.js
+++ app.js
@@ -3,11 +3,39 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var ParseServer = require('parse-server').ParseServer;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+var ParseDashboard = require('parse-dashboard');
+
+var dashboard = new ParseDashboard({
+  "apps": [
+    {
+      "serverURL": process.env.serverURL,
+      "appId": process.env.appId,
+      "masterKey": process.env.masterKey,
+      "appName": "MyApp"
+    }
+  ],
+  trustProxy: 1,
+  "users": [
+    {
+      "user":"user1",
+      "pass":"pass"
+    }
+  ]
+});
+
+var api = new ParseServer({
+  databaseURI: process.env.MONGODB_URI, // Connection string for your MongoDB database
+  appId: process.env.appId,
+  masterKey: process.env.masterKey, // Keep this key secret!
+  serverURL: process.env.serverURL // Don't forget to change to https if needed
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +49,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// make the Parse Dashboard available at /dashboard
+app.use('/dashboard', dashboard);
+
+// Serve the Parse API on the /parse URL prefix
+app.use('/parse', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
